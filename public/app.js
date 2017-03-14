723c110e49f9ccf04d6bf995c51780a59af7a3fe
Lunar Eclipse <Lunar Eclipse>
2017-03-14 14:21:02 -0500

    3/14

--- public/app.js
+++ public/app.js
@@ -8,6 +8,13 @@ xhr.onreadystatechange = function() {
   if (xhr.readyState === DONE) {
     if (xhr.status === OK) {
       console.log(xhr.responseText); // 'This is the returned text.'
+      var projects = JSON.parse(xhr.responseText);
+      projects.forEach(function(project){
+        var name=document.createElement('a');
+        name.innerHTML=project.name;
+        name.href="/projects/"+project.id;
+        document.body.appendChild(name);
+      })
     } else {
       console.log('Error: ' + xhr.status); // An error occurred during the request.
     }
